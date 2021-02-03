238585db0ea3b8413faf7d4931e22c1a91f57592
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-02-03 22:58:16 +0000

    log messages

--- dist/index.js
+++ dist/index.js
@@ -24656,6 +24656,9 @@ module.exports = envVariables;
 const github = '@actions/github';
 
 const pullRequestPayload = () => {
+  console.log('github', github);
+  console.log('context', github.context);
+
   if (!github.context.payload.pull_request) {
     throw new Error('Action can be triggered only by Pull requests');
   }
--- src/validators/payload.js
+++ src/validators/payload.js
@@ -1,6 +1,9 @@
 const github = '@actions/github';
 
 const pullRequestPayload = () => {
+  console.log('github', github);
+  console.log('context', github.context);
+
   if (!github.context.payload.pull_request) {
     throw new Error('Action can be triggered only by Pull requests');
   }
