a9e664985b8ba082c67a102546159c0022751dd6
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-02-10 23:54:26 +0000

    check params to create deployment

--- dist/index.js
+++ dist/index.js
@@ -29976,6 +29976,15 @@ const {
 } = __nccwpck_require__(6);
 
 const createDeployment = async () => {
+  console.log({
+    owner: getRepositoryOwner(),
+    repo: getRepositoryName(),
+    ref: `refs/heads/${getBranchName()}`,
+    environment: getS3BucketName(),
+    auto_merge: false,
+    transient_environment: true,
+    required_contexts: []
+  });
   const deployment = await githubClient.repos.createDeployment({
     owner: getRepositoryOwner(),
     repo: getRepositoryName(),
--- src/services/github/createDeployment.js
+++ src/services/github/createDeployment.js
@@ -7,6 +7,15 @@ const {
 } = require('../../input');
 
 const createDeployment = async () => {
+  console.log({
+    owner: getRepositoryOwner(),
+    repo: getRepositoryName(),
+    ref: `refs/heads/${getBranchName()}`,
+    environment: getS3BucketName(),
+    auto_merge: false,
+    transient_environment: true,
+    required_contexts: []
+  });
   const deployment = await githubClient.repos.createDeployment({
     owner: getRepositoryOwner(),
     repo: getRepositoryName(),
